db6e2c37df7347a96989ea8757f116a12b2e34cd
Manara <manarakozhamuratova@gmail.com>
2022-10-23 16:00:03 +0600

    init

--- index.php
+++ index.php
@@ -1,21 +1,50 @@
 <?php
 
-$url = "https://api.thecatapi.com/v1/images/search";
+require_once ('vendor/autoload.php');
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERPWD, "my_password");
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+use GuzzleHttp\Client; 
 
-$output = curl_exec($ch);
-$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$info = curl_getinfo($ch);
-curl_close($ch);
+const base_url = "https://api.thecatapi.com/";
 
-$object = json_decode( $output );
+$client = new GuzzleHttp\Client(['verify' => false]);
 
-$total_results = $object->total_results;
-$array = json_decode( $output, true );
-$total_results = $array['total_results'];
-echo $total_results;
+class Cats{
+    private $url;
+    private $client;
+
+    public function __construct($url, $client)
+    {
+        $this->url=$url;
+        $this->client=$client;
+    }
+
+    function getCategories()
+    {
+        $response = $this->client->request("GET", $this->url."v1/categories");
+        $categoriesApi = json_decode($response->getBody(), true);
+        asort($categoriesApi);
+        foreach($categoriesApi as $category){
+            echo $category["id"]." ".$category["name"]."<br>\n";
+        }
+    }
+
+    function displayImg()
+    {
+        $category = $_GET['category_ids'] ?? null;
+        if (isset($category)){
+            $catApi = "v1/images/search?category_ids=$category";
+        } else{
+            $catApi = "v1/images/search";
+        }
+
+        $result = $this->client->request("GET", $this->url.$catApi);
+        $imgApi = json_decode($result->getBody(), true);
+        foreach ($imgApi as $img){
+            return $img["url"];
+        }
+    }
+}
+
+$objCats = new Cats(base_url,$client);
+$objCats->getCategories();
+echo '<html><body><img src= "'. $objCats->displayImg() .'" /></body></html>';
